5b78ee1ad262991a37cf2926053fc02f5a91a10e
Ubuntu <klay001@ucr.edu>
2021-02-14 23:02:24 -0800

    Second part finished

--- source/main.c
+++ source/main.c
@@ -9,8 +9,42 @@
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
+#include <avr/interrupt.h>
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+unsigned char tmpB = 0x00;
+
+void TimerOn(){
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+void TimerOff(){
+	TCCR1B = 0x00;
+}
+void TimerISR(){
+	TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;	
+}
+
 
 void set_PWM(double frequency){
 	static double current_frequency;
@@ -43,27 +77,88 @@ void PWM_off(){
 	TCCR3B = 0x00;
 }
 
+
+enum States{init, off, on, scaleUp, scaleDown} state;
+
+double freq[8] = {261.63,293.66,329.63,349.23,392,440,493.88,523.25};
+unsigned char i = 0;
+
+void Tick(){
+	unsigned char tmpA = ~PINA;
+	switch(state){
+		case init: 
+			state = off; 
+			break;
+		case off: 
+			if (tmpA == 0x04){
+				PWM_on();
+				state = on;	
+			}
+			else{state = off;}
+			   break;
+		case on:
+			  if (tmpA == 0x01){state = scaleUp;}
+			  else if (tmpA  == 0x02){state = scaleDown;}
+			  else if (tmpA  == 0x04){state = off;}
+			  else{state = on;}
+			  break;
+		case scaleUp: 
+			  if(tmpA == 0x04){
+				  state = off;
+			  }
+			  else{
+				  state = on;
+			  }
+			  break;
+		case scaleDown: 
+			  if(tmpA == 0x04){
+                                  state = off;
+                          }
+                          else{
+                                  state = on;
+                          }
+    			  break;
+		default: 
+			  state = init; 
+			  break;
+	}
+
+	switch(state){
+		case off: 
+			PWM_off(); 
+			i = 0; 
+			break;
+		case on: 
+			set_PWM(freq[i]); 
+			break;
+		case scaleUp: 
+			if (i < 7){	
+				i++;
+			}	
+			break;
+		case scaleDown: 
+			if (i > 0){
+				i--;
+			}	
+			break;
+		default: 
+			break;
+
+	}
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
-	unsigned char tmpA = 0x00;
 	PWM_on();
+	TimerSet(100);
+	TimerOn();
+	
     while (1) {
-	tmpA = ~PINA & 0x07;
-	if(tmpA == 1){
-		set_PWM(261.63);
-	}
-	else if(tmpA == 2){
-		set_PWM(293.66);
-	}
-	else if(tmpA == 4){
-		set_PWM(329.63);
-	}
-	else{
-		set_PWM(0);
-	}
+	Tick();
+	while(!TimerFlag);
+	TimerFlag = 0;
     }
     PWM_off();
     return 1;
